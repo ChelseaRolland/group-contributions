048966ec3b8056639671c2380cd3e4759d10db78
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-03 15:35:38 -0500

    feat: cleaned up main.js

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    // var html = '<div class="coffee col-6">';
-    // html += '<p class="d-none">' + coffee.id + '</p>'; /* class="d-none"*/
-    // html += '<h6 class="d-inline-block pr-1">' + coffee.name + '</h6>';
-    // html += '<p class="d-inline-block text-muted sizing">' + coffee.roast + '</p>';
-    // html += '</div>';
 
     var html = '<div class="col mb-4">';
     html += '<div class="card">';
@@ -22,44 +17,6 @@ function renderCoffee(coffee) {
     html += '</div>';
     html += '</div>';
     html += '</div>';
-
-    // var html = '<div class="col mb-4">';
-    // html += '<div class="card">';
-    // html += '<img src="img/pexels-tirachard-kumtanom-544113.jpg" class="card-img" alt="coffee">';
-    //
-    // html += '</div>';
-    // html += '</div>';
-    // html += '</div>';
-    // html += '</div>';
-
-
-
-        // <div class="col mb-4">
-        //     <div class="card">
-        //         <img src="..." class="card-img-top" alt="...">
-        //             <div class="card-body">
-        //                 <h5 class="card-title">Card title</h5>
-        //                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        //             </div>
-        //     </div>
-
-    // <div class="media">
-    //     <img src="..." class="mr-3" alt="...">
-    //         <div class="media-body">
-    //             <h5 class="mt-0">Media heading</h5>
-    //             Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-    //         </div>
-    // </div>
-    // var html = '<div class="media col-6 align-items-center text-center">';
-    // html += '<img src="img/pexels-tirachard-kumtanom-544113.jpg" class="mr-3" alt="coffee" style="width: 128px;" >'
-    // html += '<div class="media-body">'
-    // html += '<h5 class="pr-1 headings">' + coffee.name + '</h5>';
-    // html += '<p class="text-muted sizing paratags">' + coffee.roast + '</p>';
-    // html += '</div>'
-    // html += '</div>'
-    // html += '</div>'
-    // html += '</div>'
-
 
     return html;
 }
@@ -94,29 +51,6 @@ function updateCoffeesTRY(e) {
     }
     tbody.innerHTML = renderCoffees(newArr);
 }
-
-// function updateCoffees2(e) {
-//     e.preventDefault(); // don't submit the form, we just want to update the data
-//     var selectedRoast = roastSelection2.value;
-//     let inputedCoffee = (coffeeSelection2.value).toLowerCase();
-//     var filteredCoffees = [];
-//     var newArr = [];
-//
-//     for (let i = 0; i < coffees.length; i++) {
-//         let roast = coffees[i].roast;
-//         if(roast === selectedRoast) {
-//             filteredCoffees.push(coffees[i]);
-//         }
-//     }
-//
-//     for (let i = 0; i < filteredCoffees.length;  i++) {
-//         let name = (filteredCoffees[i].name).toLowerCase();
-//         if (name === inputedCoffee || name.includes(inputedCoffee)){
-//             newArr.push(filteredCoffees[i])
-//         }
-//     }
-//     tbody.innerHTML = renderCoffees(newArr);
-// }
 
 function addCoffee (e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
@@ -164,7 +98,7 @@ var roastSelection2 = document.querySelector('#add-roast-selection');
 var coffeeSelection = document.querySelector('#coffeeName');
 var coffeeSelection2 = document.querySelector('#add-coffeeName');
 
-// console.log("Pre-sort", coffees);
+
 
 coffees.sort(function (a,b) {
     return b.id - a.id;
@@ -172,7 +106,6 @@ coffees.sort(function (a,b) {
 
 tbody.innerHTML = renderCoffees(coffees);
 
-// submitButton.addEventListener('click', addCoffee);
 submitButton2.addEventListener('click', addCoffee);
 
 roastSelection.addEventListener('change', updateCoffeesTRY);
