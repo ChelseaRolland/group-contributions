ad3821e08db4f8ab477d12d8b5486e87fe3aeb9e
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-03 16:14:55 -0500

    feat: cards styled, font updated, sizing adjusted

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 "use strict"
 
 function renderCoffee(coffee) {
+    var cardPicture = '';
+    function pickPic(object){
+        if (object.roast === "light") {
+            cardPicture = "img/light-coffee.jpeg"
+        } else if (object.roast === "medium") {
+            cardPicture = "img/medium-coffee.jpeg"
+        } else {
+            cardPicture = "img/dark-coffee.jpeg"
+        }
+        return cardPicture;
+    }
+
+    // console.log(pickPic(coffee));
 
     var html = '<div class="col mb-4">';
-    html += '<div class="card">';
+    html += '<div class="card p-2">';
     html += '<div class="row no-gutters">';
     html += '<div class="col">';
-    html += '<img src="img/pexels-tirachard-kumtanom-544113.jpg" class="card-img" alt="coffee">';
+    html += '<img src="' + pickPic(coffee) + '" class="card-img" alt="coffee">';
     html += '</div>'
     html += '<div class="col-md-8">'
-    html += '<div class="card-body">';
-    html += '<h6 class="d-inline-block headings pr-2">' + coffee.name + '</h6>';
-    html += '<p class="d-inline-block sizing">' + coffee.roast + '</p>';
+    html += '<div class="card-body p-0 text-center h-100 d-flex justify-content-center flex-column">';
+    html += '<h4 class="headings">' + coffee.name + '</h4>';
+    html += '<p class="sizing">' + coffee.roast + '</p>';
     html += '</div>';
     html += '</div>';
     html += '</div>';
